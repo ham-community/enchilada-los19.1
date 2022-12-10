42cbbf0b4a6ae5ced08ec31ec73f30b06bd62b00
antony-jr <antonyjr@protonmail.com>
2022-12-10 13:29:18 +0530

    [REMOVE] Don't include F-Droid APK directly.

--- scripts/add-vendor-extra.sh
+++ scripts/add-vendor-extra.sh
@@ -2,7 +2,8 @@
 
 mkdir -p /ham-build/android/vendor/extra
 
-#echo "# Override the updater URL." >> /ham-build/android/vendor/extra/product.mk
-#echo 'PRODUCT_PROPERTY_OVERRIDES += \' >> /ham-build/android/vendor/extra/product.mk
-#echo '    lineage.updater.uri='$UPDATER_URL'' >> /ham-build/android/vendor/extra/product.mk
+echo "# Override the updater URL." >> /ham-build/android/vendor/extra/product.mk
+echo 'PRODUCT_PROPERTY_OVERRIDES += \' >> /ham-build/android/vendor/extra/product.mk
+echo '    lineage.updater.uri='$UPDATER_URL'' >> /ham-build/android/vendor/extra/product.mk
+echo "" >> /ham-build/android/vendor/extra/product.mk
 cat /ham-recipe/source/product.mk >> /ham-build/android/vendor/extra/product.mk
